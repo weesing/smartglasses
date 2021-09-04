c28d0c726c5be300fa15b69951aa7701a484dd2c
WeeSing <posttomichael@gmail.com>
2021-09-04 09:50:09 +0800

    Added sample BMP for ESP32. Added arduino test display code

---
+++ src/arduino_nano_ble/README.md
@@ -0,0 +1,11 @@
+# Pin Assignment
+| ST7735S | Arduino Nano BLE
+| -- | --
+| VCC | 5v
+| GND | GND
+| SDA | 11
+| SCK | 13
+| RST | 8
+| DC | 9
+| CS | 10
+| BLK | 3v3
--- src/esp32/main.py
+++ src/esp32/main.py
@@ -105,11 +105,48 @@ async def displayTiles():
             print('Fill rect at ', (x, y), 'with color', hex(r), hex(g), hex(b))
             tft.fillrect((x, y), (tileWidth, tileHeight), TFTColor(r, g, b))
 
+async def displayImage():
+    tft.fill(TFT.BLACK)
+    f = open('flower128x160.bmp', 'rb')
+    if f.read(2) == b'BM':  # header
+        dummy = f.read(8)  # file size(4), creator bytes(4)
+        offset = int.from_bytes(f.read(4), 'little')
+        hdrsize = int.from_bytes(f.read(4), 'little')
+        width = int.from_bytes(f.read(4), 'little')
+        height = int.from_bytes(f.read(4), 'little')
+        if int.from_bytes(f.read(2), 'little') == 1:  # planes must be 1
+            depth = int.from_bytes(f.read(2), 'little')
+            # compress method == uncompressed
+            if depth == 24 and int.from_bytes(f.read(4), 'little') == 0:
+                print("Image size:", width, "x", height)
+                rowsize = (width * 3 + 3) & ~3
+                if height < 0:
+                    height = -height
+                    flip = False
+                else:
+                    flip = True
+                w, h = width, height
+                if w > 160:
+                    w = 160
+                if h > 80:
+                    h = 80
+                tft._setwindowloc((0, 0), (w - 1, h - 1))
+                for row in range(h):
+                    if flip:
+                        pos = offset + (height - 1 - row) * rowsize
+                    else:
+                        pos = offset + row * rowsize
+                    if f.tell() != pos:
+                        dummy = f.seek(pos)
+                    for col in range(w):
+                        bgr = f.read(3)
+                        tft._pushcolor(TFTColor(bgr[2], bgr[1], bgr[0]))
 
 async def displayLoop():
     while True:
         uasyncio.create_task(displayTiles())
-        await uasyncio.sleep_ms(5000)
+        uasyncio.create_task(displayImage())
+        await uasyncio.sleep_ms(10000)
 
 
 uasyncio.run(displayLoop())
